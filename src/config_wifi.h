ce387c6de8152c69a67ba76090ef97d0d74830bf
Minoru Tomobe <minoru.tomobe@gmail.com>
2021-08-15 15:01:20 +0900

    Code cleanup.

--- src/config_wifi.h
+++ src/config_wifi.h
@@ -1,19 +1,19 @@
 /* WiFi & IP Address Configrations */
 /* SSID & Password */
+/* SSID & Password */
 const char *ssid[] = {
     "ESP32-WiFiKey", /* Standalone */
-    "YOURSSID"   /* WiFi AP SSID */
+    "YOURSSID"       /* WiFi AP SSID */
 };
-
 const char *passwd[] = {
-    "wifikey32",    /* Standalone */
+    "wifikey32", /* Standalone */
     "YOURPASSWD" /* WiFi AP Password */
 };
 
 /* IP Addresses */
 IPAddress server(192, 168, 1, 192);
+IPAddress ap_server(192, 168, 4, 1);
 IPAddress client(192, 168, 4, 2);
-IPAddress gateway(192, 168, 4, 1);
 IPAddress subnet(255, 255, 255, 0);
 int httpport = 80;
-int udpport = 9000;
+int udpport = 56000;
